0aaca313639b1117f6e1a6bbdb2b21bbf0eca0c0
Cooldude2606 <cooldude260607@gmail.com>
2021-01-30 22:16:21 +0000

    Added Rule Output

--- apps/communitybot/src/utils/messages.ts
+++ apps/communitybot/src/utils/messages.ts
@@ -8,3 +8,19 @@ export function sendReply(message: Discord.Message, text: string) {
 export function sendResult(message: Discord.Message, text: string) {
     message.channel.send('\n'+text)
 }
+
+export function sendLines(message: Discord.Message, liens: string[]) {
+    const messages: string[] = []
+    let currentMessage: string = ''
+    liens.forEach(line => {
+        if (line.length > 2000) throw new Error('Line is longer than 2000 characters')
+        if (currentMessage.length + line.length > 2000) {
+            messages.push(currentMessage)
+            currentMessage = ''
+        }
+        currentMessage += '\n'+line
+    })
+
+    messages.push(currentMessage)
+    messages.forEach(nextMessage => message.channel.send(nextMessage))
+}
