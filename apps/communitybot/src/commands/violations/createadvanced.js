003c08762e073f60d02c52f8ba620d4237ecb604
oof2win2 <honza.koco44@gmail.com>
2021-04-20 22:04:24 +0200

    Separating creating violation into two, comments

--- apps/communitybot/src/commands/violations/createadvanced.js
+++ apps/communitybot/src/commands/violations/createadvanced.js
@@ -6,8 +6,8 @@ const { getMessageResponse } = require("../../utils/responseGetter")
 
 module.exports = {
     config: {
-        name: "create",
-        aliases: ["createviolation", "ban", "banhammer"],
+        name: "createadvanced",
+        aliases: [],
         usage: "",
         category: "violations",
         description: "Creates a violation for a player",
@@ -26,18 +26,25 @@ module.exports = {
 
         const playername = (await getMessageResponse(message.channel.send("Please type in a playername for the violation"), messageFilter))?.content
         if (playername === undefined) return message.channel.send("Didn't send playername in time")
-        
+
+        const adminname = (await getMessageResponse(message.channel.send("Please type in admin name for the violation"), messageFilter))?.content
+        if (adminname === undefined) return message.channel.send("Didn't send adminname in time")
+
         const ruleid = (await getMessageResponse(message.channel.send("Please type in ObjectID of rule that has been broken"), messageFilter))?.content
         if (ruleid === undefined) return message.channel.send("Didn't send rule ObjectID in time")
 
         let desc = (await getMessageResponse(message.channel.send("Please type in description of the violation or `none` if you don't want to set one"), messageFilter))?.content
         if (desc.toLowerCase() === 'none') desc = undefined
-        
+
         let proof = (await getMessageResponse(message.channel.send("Please send a link to proof of the violation or `none` if there is no proof"), messageFilter))?.content
         if (proof.toLowerCase() === 'none') proof = undefined
 
-        const timestamp = (new Date).toISOString()
-        const adminname = message.author.tag
+        let timestamp = (await getMessageResponse(message.channel.send("Please send a value representing the date of the violation. Type in `now` to set the current time"), messageFilter))?.content
+        if (timestamp.toLowerCase() === 'now') timestamp = (new Date).toISOString()
+        else {
+            if (isNaN(Date.parse(timestamp))) timestamp = (new Date).toISOString()
+            else timestamp = Date.parse(timestamp).toISOString()
+        }
 
         let embed = new MessageEmbed()
             .setTitle("FAGC Violations")
@@ -50,25 +57,22 @@ module.exports = {
             { name: "Player name", value: playername, inline: true },
             { name: "Rule ID", value: ruleid, inline: true },
             { name: "Violation description", value: desc, inline: true },
-            { name: "Proof", value: proof},
-            { name: "Violated At (ISO)", value: timestamp}
+            { name: "Proof", value: proof },
+            { name: "Violated At (ISO)", value: timestamp }
         )
         message.channel.send(embed)
         const confirm = await message.channel.send("Do you wish to create this rule violation?")
         confirm.react("✅")
         confirm.react("❌")
-        
         let reactions
         try {
             reactions = (await confirm.awaitReactions(reactionFilter, { max: 1, time: 120000, errors: ['time'] }))
         } catch {
             return message.channel.send("Timed out.")
         }
-
         let reaction = reactions.first()
         if (reaction.emoji.name === "❌")
             return message.channel.send("Violation creation cancelled")
-        
         try {
             const responseRaw = await fetch(`${apiurl}/violations/create`, {
                 method: "POST",
