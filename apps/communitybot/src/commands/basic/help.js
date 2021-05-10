5653c7bba44dd7c131d39eab334d1058418ef009
oof2win2 <honza.koco44@gmail.com>
2021-05-10 20:33:32 +0200

    :bug: Fixed help command

--- apps/communitybot/src/commands/basic/help.js
+++ apps/communitybot/src/commands/basic/help.js
@@ -1,14 +1,16 @@
-const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
 const { readdirSync } = require("fs");
 const { stripIndents } = require("common-tags");
 const Command = require("../../base/Command")
+
 class Help extends Command {
   constructor(client) {
     super(client, {
       name: "help",
       description: "Displays all available commands",
       aliases: ["h", "commands"],
-      usage: ["{{p}}help", "{{p}}help ping"],
+      usage: "(command)",
+      examples: ["{{p}}help", "{{p}}help ping"],
       category: "basic",
       dirname: __dirname,
       enabled: true,
@@ -24,70 +26,57 @@ class Help extends Command {
     })
   }
   async run(message, args) {
-    const embed = new MessageEmbed()
-      .setColor(this.client.config.embeds.color)
-      .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL)
-      .setThumbnail(this.client.user.displayAvatarURL());
+    const prefix = this.client.config.prefix
+    if (args[0]) {
+      const cmd = this.client.commands.get(args[0]) || this.client.commands.get(this.client.aliases.get(args[0]));
+      if (!cmd) {
+        return message.error(`\`${args[0]}\` is not a valid command\nType \`${this.client.config.prefix}help\` to see a list of available commands!`);
+      }
 
-    if (!args[0]) {
-      const categories = readdirSync("./commands/");
+      const description = cmd.help.description ? cmd.help.description : "No description";
+      const usage = cmd.help.usage ? "```\n" + prefix + cmd.help.name + " " + cmd.help.usage + "\n```" : prefix + cmd.help.name;
+      const examples = cmd.help.examples ? `\`\`\`${cmd.help.examples.join("\n").replace(/\{\{p\}\}/g, prefix)}\`\`\`` : `\`\`\`${prefix}${cmd.help.name}\`\`\``;
+      // Creates the help embed
+      const groupEmbed = new Discord.MessageEmbed()
+        .setAuthor(`${prefix}${cmd.help.name} help`)
+        .addField("Description", description)
+        .addField("Usage", usage)
+        .addField("Examples", examples)
+        .addField("Aliases", cmd.help.aliases.length > 0
+          ? cmd.help.aliases.map(a => `\`${a}\``).join(",")
+          : "No aliases"
+        )
+        .addField("Member permissions requried", cmd.conf.memberPermissions.length > 0
+          ? cmd.conf.memberPermissions.map((p) => "`" + p + "`").join("\n")
+          : "No specific permission is required to execute this command"
+        )
+        .setColor(this.client.config.embeds.color)
+        .setFooter(this.client.config.embeds.footer);
 
-      embed.setDescription(
-        `These are the avaliable commands for ${message.guild.me.displayName}\nThe bot prefix is: **${this.client.config.prefix}**`
-      );
-      embed.setFooter(
-        `© ${message.guild.me.displayName} | Developed by DistroByte | Total Commands: ${this.client.commands.size}`,
-        this.client.user.displayAvatarURL()
-      );
-
-      categories.forEach((category) => {
-        const dir = this.client.commands.filter(
-          (c) => c.help.category === category
-        );
-        const capitalise =
-          category.slice(0, 1).toUpperCase() + category.slice(1);
-        try {
-          embed.addField(
-            `${capitalise} [${dir.size}]:`,
-            dir.map((c) => `\`${c.help.name}\``).join(" ")
-          );
-        } catch (e) {  }
-      });
-
-      return message.channel.send(embed);
-    } else {
-      let command = this.client.commands.get(
-        this.client.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase()
-      );
-      if (!command)
-        return message.channel.send(
-          embed
-            .setTitle("Invalid Command.")
-            .setDescription(
-              `Do \`${this.client.config.prefix}help\` for the list of the commands.`
-            )
-        );
-      command = command.config;
-
-      embed.setDescription(stripIndents`The bot's prefix is: \`${this.client.config.prefix}\`\n
-            **Command:** ${command.name.slice(0, 1).toUpperCase() + command.name.slice(1)
-        }
-            **Description:** ${command.description || "No Description provided."
-        }
-            **Usage:** ${command.usage
-          ? `\`${this.client.config.prefix}${command.name} ${command.usage}\``
-          : `\`${this.client.config.prefix}${command.name}\``
-        }
-            **Accessible by:** ${command.accessableby || "Members"}
-            **Aliases:** ${command.aliases ? command.aliases.join(", ") : "None"
-        }`);
-      embed.setFooter(
-        `© ${message.guild.me.displayName} | Developed by DistroByte`,
-        this.client.user.displayAvatarURL()
-      );
-
-      return message.channel.send(embed);
+      // and send the embed in the current channel
+      return message.channel.send(groupEmbed);
     }
+
+    const categories = [];
+    const commands = this.client.commands;
+
+    commands.forEach((command) => {
+      if (!categories.includes(command.help.category)) {
+        if (command.help.category === "Owner" && message.author.id !== this.client.config.owner.id) {
+          return;
+        }
+        categories.push(command.help.category);
+      }
+    });
+
+    const embed = new Discord.MessageEmbed()
+      .setDescription(`● To get help on a specific command type\`${prefix}help <command>\`!`)
+      .setColor(this.client.config.embeds.color)
+      .setFooter(this.client.config.embeds.footer);
+    categories.sort().forEach((cat) => {
+      const tCommands = commands.filter((cmd) => cmd.help.category === cat);
+      embed.addField(cat + " - (" + tCommands.size + ")", tCommands.map((cmd) => "`" + cmd.help.name + "`").join(", "));
+    });
   }
 }
 
