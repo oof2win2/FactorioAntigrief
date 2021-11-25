c1f11b46c2e74458711fad1c509c4201efc9227c
oof2win2 <honza.koco44@gmail.com>
2021-11-25 23:04:50 +0100

    :wastebasket: further prettier removal

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
+/* eslint-disable array-bracket-spacing */
+
+
 module.exports = {
 	env: {
 		commonjs: true,
 		es2021: true,
 		node: true,
 	},
-	extends: ["eslint:recommended", "prettier"],
+	extends: ["eslint:recommended"],
 	parserOptions: {
 		ecmaVersion: 12,
 	},
@@ -13,5 +16,7 @@ module.exports = {
 		"linebreak-style": ["error", "unix"],
 		quotes: ["error", "double"],
 		semi: ["error", "never"],
+		"object-curly-spacing": ["error", "always"],
+		"array-bracket-spacing": ["error", "always"],
 	},
 }
