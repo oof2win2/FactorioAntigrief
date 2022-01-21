08b9357708e4f025ed9f4a1fd14795b9eb2fbd31
oof2win2 <honza.koco44@gmail.com>
2022-01-21 21:33:55 +0100

    feat(commands): fetching rules

---
+++ apps/communitybot/src/commands/rules/allrules.ts
@@ -0,0 +1,79 @@
+// const { MessageEmbed } = require("discord.js")
+// const Command = require("../../base/Command")
+// const { createPagedEmbed } = require("../../utils/functions")
+
+import { EmbedField, MessageEmbed } from "discord.js";
+import { Command } from "../../base/Command";
+import { createPagedEmbed } from "../../utils/functions";
+
+// class GetAllRules extends Command {
+// 	constructor(client) {
+// 		super(client, {
+// 			name: "allrules",
+// 			description: "Gets all rules",
+// 			category: "rules",
+// 			dirname: __dirname,
+// 			enabled: true,
+// 			memberPermissions: [],
+// 			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+// 			ownerOnly: false,
+// 			cooldown: 3000,
+// 		})
+// 	}
+// 	async run(message) {
+// 		const rules = await this.client.fagc.rules.fetchAll({})
+
+// 		let embed = new MessageEmbed()
+// 			.setTitle("FAGC Rules")
+// 			.setColor("GREEN")
+// 			.setTimestamp()
+// 			.setAuthor("FAGC Community")
+// 			.setDescription("All FAGC Rules")
+// 		let fields = []
+// 		for (let i = 0; i < rules.length; i += 2) {
+// 			fields.push({
+// 				name: `${rules[i].shortdesc} (\`${rules[i].id}\`)`,
+// 				value: rules[i + 1] ? `**${rules[i + 1].shortdesc}** (\`${rules[i + 1].id}\`)` : "\u200b",
+// 			})
+// 		}
+
+// 		createPagedEmbed(fields, embed, message, { maxPageCount: 10 })
+// 	}
+// }
+// module.exports = GetAllRules
+
+
+const AllRules: Command = {
+	name: "allrules",
+	description: "Gets all rules",
+	category: "rules",
+	aliases: [],
+	usage: "",
+	examples: [],
+	requiresRoles: false,
+	requiresApikey: false,
+	run: async ({client, message}) => {
+		// fetch rules from backend
+		const rules = await client.fagc.rules.fetchAll({})
+		// create a nice embed to display the rules
+		const embed = new MessageEmbed()
+			.setTitle("FAGC Rules")
+			.setColor("GREEN")
+			.setTimestamp()
+			.setAuthor({name: client.config.embeds.author})
+			.setFooter({text: client.config.embeds.footer})
+			.setDescription("All FAGC Rules")
+		// create the fields for the embed
+		const fields: EmbedField[] = []
+		for (let i = 0; i < rules.length; i += 2) {
+			fields.push({
+				name: `${rules[i].shortdesc} (\`${rules[i].id}\`)`,
+				// this is done so that two rules are per field to take up less space
+				value: rules[i + 1] ? `**${rules[i + 1].shortdesc}** (\`${rules[i + 1].id}\`)` : "\u200b",
+				inline: false
+			})
+		}
+		createPagedEmbed(fields, embed, message, { maxPageCount: 10 })
+	}
+}
+export default AllRules
