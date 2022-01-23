674b798ef37d0b20e16de66302293fc1c2973b00
oof2win2 <honza.koco44@gmail.com>
2022-01-23 02:04:49 +0100

    feat(commands): create + view reports (basic)

---
+++ src/commands/reports/allreports.ts
@@ -0,0 +1,53 @@
+import { MessageEmbed } from "discord.js";
+import { Command } from "../../base/Command";
+import { createPagedEmbed } from "../../utils/functions";
+import { getMessageResponse } from "../../utils/responseGetter";
+
+const AllReports: Command = {
+	name: "allreports",
+	description: "Gets all reports of a player",
+	aliases: [ "checkall", "viewallreports" ],
+	category: "reports",
+	usage: "[playername]",
+	examples: [ "allreports Windsinger" ],
+	requiresRoles: false,
+	requiresApikey: false,
+	run: async ({client, message, args}) => {
+		// if a playername is not provided as an arg, prompt the user to provide one
+		if (!args[0])
+			args = await getMessageResponse(message, `${client.emotes.type} Provide a player name to get reports of`)
+				.then((x) => x?.content?.split(" ") || [])
+		
+		const playername = args.shift()
+		if (!playername) return message.channel.send(`${client.emotes.warn} No player name was provided`)
+
+		const reports = await client.fagc.reports.search({playername: playername})
+		if (!reports[0]) return message.channel.send(`Player \`${playername}\` doesn't have any reports`)
+		console.log(reports.length)
+		const embed = new MessageEmbed()
+			.setTitle("FAGC Reports")
+			.setColor("ORANGE")
+			.setTimestamp()
+			.setAuthor({name: client.config.embeds.author})
+			.setFooter({text: client.config.embeds.footer})
+			.setDescription(`FAGC Reports of player \`${playername}\``)
+		const fields = await Promise.all(
+			reports.map(async (report) => {
+				const admin = await client.users.fetch(report.adminId).catch(() => null)
+				const rule = await client.fagc.rules.fetchRule({ ruleid: report.brokenRule })
+				const community = await client.fagc.communities.fetchCommunity({ communityId: report.communityId })
+				return {
+					name: report.id,
+					value: `By: <@${admin?.id}> | ${admin?.tag}\nCommunity ID: ${community?.name} (${community?.id})\n` +
+						`Description: ${report.description}\nAutomated: ${report.automated}\n` +
+						`Broken rule: ${rule?.shortdesc} (${rule?.id})\nProof: ${report.proof}\n` +
+						`Violated time: <t:${Math.floor(report.reportedTime.valueOf() / 1000)}>`,
+					inline: true,
+				}
+			})
+		)
+		createPagedEmbed(fields, embed, message, { maxPageCount: 5 })
+	}
+}
+
+export default AllReports
---
+++ src/commands/reports/create.ts
@@ -0,0 +1,143 @@
+import { EmbedField, MessageEmbed } from "discord.js";
+import { Command } from "../../base/Command";
+import { createPagedEmbed } from "../../utils/functions";
+import { getConfirmationMessage, getMessageResponse } from "../../utils/responseGetter";
+
+const CreateReport: Command = {
+	name: "createreport",
+	description: "Create a report for a player",
+	category: "reports",
+	aliases: [ "create", "ban" ],
+	usage: "[player] [...description]",
+	examples: ["create", "create Potato", "create Potato hacking"],
+	requiresRoles: true,
+	requiredPermissions: ["reports"],
+	requiresApikey: true,
+	run: async ({client, message, args, guildConfig}) => {
+		const playername = args.shift() ||
+			await getMessageResponse(
+				message,
+				`${client.emotes.type} Type in the player name`
+			).then(m => m?.content.split(" ").shift())
+		if (!playername) return message.channel.send("Player name not specified")
+
+		// send a message with the community's filtered rules to pick from
+		const embed = new MessageEmbed()
+			.setTitle("FAGC Reports")
+			.setColor("RED")
+			.setTimestamp()
+			.setAuthor({name: client.config.embeds.author})
+			.setFooter({text: client.config.embeds.footer})
+			.setDescription("Your community's filtered rules")
+		const allRules = await client.fagc.rules.fetchAll({})
+		const fields: EmbedField[] = allRules
+				// make sure the rule is filtered
+				.filter((rule) => guildConfig.ruleFilters.includes(rule.id))
+				// sort the rules by their index
+				.sort((a, b) => {
+					if (guildConfig.ruleFilters.indexOf(a.id) > guildConfig.ruleFilters.indexOf(b.id)) return 1
+					if (guildConfig.ruleFilters.indexOf(a.id) < guildConfig.ruleFilters.indexOf(b.id)) return -1
+					return 0
+				})
+				.map((rule) => {
+					return {
+						name: `${guildConfig.ruleFilters.indexOf(rule.id)+1}) ${rule.shortdesc} (\`${rule.id}\`)`,
+						value: rule.longdesc,
+						inline: false
+					}
+				})
+		createPagedEmbed(fields, embed, message)
+
+		const rules = await getMessageResponse(
+			message,
+			`${client.emotes.type} Type in the rule(s) broken by the player, separated with spaces, or \`none\` if there is no rule`
+		)
+		if (!rules) return message.channel.send("Rule(s) not specified")
+		const ruleIds = rules.content.split(" ")
+		if (ruleIds.length === 1 && ruleIds[0] === "none") {
+			return message.channel.send("Rule(s) not specified")
+		}
+		// check for validity of rules, sort into valid and invalid IDs
+		const invalidRuleIds: string[] = []
+		const validRuleIDs: string[] = []
+		ruleIds.map((ruleId) => {
+			let id: string
+			if (isNaN(Number(ruleId))) {
+				// id is string
+				id = ruleId
+			} else {
+				// id is index in rule filters
+				const i = Number(ruleId)
+				if (i < 0 || i > guildConfig.ruleFilters.length) {
+					return invalidRuleIds.push(ruleId)
+				}
+				const found = guildConfig.ruleFilters[i - 1]
+				id = found
+			}
+			// all rules are fetched above so they are cached
+			const rule = client.fagc.rules.resolveID(id)
+			if (!rule) invalidRuleIds.push(id)
+			else validRuleIDs.push(id)
+		})
+		if (invalidRuleIds.length) return message.channel.send(`Invalid rule(s): \`${invalidRuleIds.join("`, `")}\``)
+
+		let desc = args.join(" ") ||
+			await getMessageResponse(
+				message,
+				`${client.emotes.type} Type in description of the report or \`none\` if you don't want to set one`
+			).then((m) => m?.content)
+		if (!desc || desc.toLowerCase() === "none") desc = undefined
+
+		let proof =
+			await getMessageResponse(
+				message,
+				`${client.emotes.type} Send links to proof of the report, separated with spaces, or \`none\` if there is no proof`
+			).then(x => x?.content)
+		if (!proof || proof.toLowerCase() === "none") proof = undefined
+
+		const timestamp = Date.now()
+
+		// send an embed to display the report that will be created
+		const checkEmbed = new MessageEmbed()
+			.setTitle("FAGC Reports")
+			.setColor("RED")
+			.setTimestamp()
+			.setAuthor({name: client.config.embeds.author})
+			.setFooter({text: client.config.embeds.footer})
+			// .setDescription(`**Report created by ${message.author.tag}**\n\n**Player:** ${playername}\n**Rule(s):** ${validRuleIDs.join(", ")}\n**Description:** ${desc}\n**Proof:** ${proof}`)
+			.addFields([
+				{name: "Player", value: playername, inline: true},
+				{name: "Rule(s)", value: validRuleIDs.map(id => `${client.fagc.rules.resolveID(id)?.shortdesc} (\`${id}\`)`).join(", "), inline: true},
+				{name: "Description", value: desc || "No description", inline: true},
+				{name: "Proof", value: proof || "No proof", inline: true},
+				{name: "Reported at", value: `<t:${Math.round(timestamp / 1000)}>`, inline: true}
+			])
+		message.channel.send({
+			embeds: [checkEmbed]
+		})
+		const confirmationMessage = await getConfirmationMessage(message, "Are you sure you want to create these reports?")
+		if (!confirmationMessage) return message.channel.send("Report creation cancelled")
+
+		// create the reports for each rule
+		const reports = await Promise.all(
+			validRuleIDs.map(async (ruleId) => {
+				return client.fagc.reports.create({
+					report: {
+						playername: playername,
+						adminId: message.author.id,
+						description: desc ?? "No description",
+						proof: proof ?? "No proof",
+						brokenRule: ruleId,
+						reportedTime: new Date(timestamp),
+						automated: false
+					},
+					reqConfig: {
+						apikey: guildConfig.apiKey || ""
+					}
+				})
+			})
+		)
+		return message.channel.send(`Reports created with IDs: ${reports.map(report => `\`${report.id}\``).join(", ")}`)
+	}
+}
+export default CreateReport
