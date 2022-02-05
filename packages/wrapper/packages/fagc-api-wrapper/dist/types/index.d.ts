621c9adc3e35385dbf8be4ef8b95ac3ba71fd75f
oof2win2 <honza.koco44@gmail.com>
2022-02-05 20:43:07 +0100

    feat: add base FAGC project

--- packages/wrapper/packages/fagc-api-wrapper/dist/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./types";
-export * from "./errors";
-//# sourceMappingURL=index.d.ts.map
--- packages/wrapper/packages/fagc-api-wrapper/dist/types/privatetypes.d.ts
+++
@@ -1,7 +0,0 @@
-import { RequestConfig } from ".";
-export interface FetchRequestTypes {
-    reqConfig?: RequestConfig;
-    force?: boolean;
-    cache?: boolean;
-}
-//# sourceMappingURL=privatetypes.d.ts.map
