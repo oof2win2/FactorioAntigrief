943dc4c2d116e46c419d84bc63faaa0a026584cc
oof2win2 <honza.koco44@gmail.com>
2022-02-09 17:18:56 +0100

    fix: tests timing out in some cases

--- apps/backend/test/prepareTest.ts
+++ apps/backend/test/prepareTest.ts
@@ -6,20 +6,20 @@ import { createCategories } from "./utils"
 import backend from "../src/app"
 
 
-let mongod: MongoMemoryReplSet;
+let mongod: MongoMemoryReplSet
 export let testCommunity: CommunityClass
 export let testCategories: CategoryClass[]
 beforeAll(async () => {
 	// ephemeralForTest does not support change streams
-	mongod = await MongoMemoryReplSet.create({ instanceOpts: [{ storageEngine: "wiredTiger" }]})
+	mongod = await MongoMemoryReplSet.create({ instanceOpts: [ { storageEngine: "wiredTiger" } ] })
 	await mongoose.connect(mongod.getUri(), { ignoreUndefined: true })
-
+	
 	// Insert test data
 	testCommunity = await CommunityModel.create({ name: "Test Community", contact: "12345" })
 	testCategories = await CategoryModel.create(createCategories(3))
-
 	await backend.listen(0)
-}, 20e3)
+// TODO: reduce to 20s once MongoMemoryReplSet takes less time to startup
+}, 60e3)
 
 afterAll(async () => {
 	// Watchers throw when disconnecting for some reason
