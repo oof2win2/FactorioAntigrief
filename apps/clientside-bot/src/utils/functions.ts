aa26fd86c39a149bb6f6c19d6b5177c9b2f398d3
oof2win2 <honza.koco44@gmail.com>
2022-02-21 20:42:31 +0000

    feat(clientside): add function to split array into groups

--- apps/clientside-bot/src/utils/functions.ts
+++ apps/clientside-bot/src/utils/functions.ts
@@ -34,3 +34,27 @@ export async function sendGuildMessage(
 		owner()
 	}
 }
+
+/**
+ * Split a single large array into multiple smaller ones
+ * @param items A single array of items to split into smalleer groups of the specified size
+ * @param maxCount Maximum items in a group
+ * @returns An array of arrays of items, with the maximum number of items per group being the specified size
+ */
+export function splitIntoGroups<T>(items: T[], maxCount = 500): T[][] {
+	return items.reduce<T[][]>(
+		(previous, item) => {
+			const last = previous[previous.length - 1]
+			if (last.length >= maxCount) {
+				// if the last group is full, start a new one
+				previous.push([item])
+			} else {
+				// otherwise, add the item to the last group
+				last.push(item)
+			}
+			return previous
+		},
+		[[]]
+	)
+}
+
