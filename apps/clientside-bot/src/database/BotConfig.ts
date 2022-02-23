e398c4b1fa25279b850489221bfb1e272ad30bd0
oof2win2 <honza.koco44@gmail.com>
2022-02-23 20:32:10 +0000

    fix(clientside): default BotConfig last processed Add default value to BotConfig lastNotificationProcessed Fix storage of said property to be correct

--- apps/clientside-bot/src/database/BotConfig.ts
+++ apps/clientside-bot/src/database/BotConfig.ts
@@ -24,20 +24,28 @@ export default class BotConfig {
 	apikey!: string | null
 
 	@Column({
-		type: "datetime",
-		default: () => new Date(),
+		type: "text",
+		length: 24, // max length of a date string
+		transformer: {
+			// assuming nobody messes with the database, x is always a valid ISO8601 string
+			from: (x: string) => new Date(x),
+			// if the date is valid, then set it to the ISO string. if it isn't, set it as the current date
+			to: (x) => isNaN(new Date(x).valueOf()) ? new Date().toISOString() : new Date(x).toISOString()
+		}
 	})
 	lastNotificationProcessed!: Date
 
 	@Column({
 		type: "simple-enum",
 		enum: ["none", "ban", "custom"],
+		default: "none",
 	})
 	reportAction!: "none" | "ban" | "custom"
 
 	@Column({
 		type: "simple-enum",
 		enum: ["none", "unban", "custom"],
+		default: "none",
 	})
 	revocationAction!: "none" | "unban" | "custom"
 }
