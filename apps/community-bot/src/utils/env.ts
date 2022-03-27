a3d29e492f15b081618900824d2a9df56e2d0819
oof2win2 <honza.koco44@gmail.com>
2022-03-27 16:08:14 +0000

    fix(community): add outDir, change dateformat to v4

--- apps/community-bot/src/utils/env.ts
+++ apps/community-bot/src/utils/env.ts
@@ -7,7 +7,7 @@ const ENV = cleanEnv(process.env, {
 	APIURL: url({ desc: "API URL", default: "https://factoriobans.club/api" }),
 	MASTERAPIKEY: str({ desc: "FAGC Master API key" }),
 	DISCORD_BOTTOKEN: str({ desc: "Your Discord bot token" }),
-	SENTRY_LINK: url({ desc: "Your sentry.io link" }),
+	SENTRY_LINK: str({ desc: "Your sentry.io link", default: "" }),
 	FAGC_INVITE_STRING: str({ desc: "FAGC server invite string" }),
 	BOTPREFIX: str({ desc: "Discord bot prefix" }),
 })
