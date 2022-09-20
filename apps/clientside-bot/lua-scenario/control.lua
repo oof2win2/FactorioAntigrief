c959f176fd0e5c2d76e96f489bb7702a8577e2e0
oof2win2 <honza.koco44@gmail.com>
2022-09-20 09:56:38 +0000

    fix(lua): newline

--- apps/clientside-bot/lua-scenario/control.lua
+++ apps/clientside-bot/lua-scenario/control.lua
@@ -4,11 +4,11 @@ script.on_init(function ()
 end)
 
 script.on_event(defines.events.on_player_banned, function (e)
-	local text = "ban;" .. e.player_name .. ";" .. (e.by_player or "") .. ";" .. (e.reason or "")
+	local text = "ban;" .. e.player_name .. ";" .. (e.by_player or "") .. ";" .. (e.reason or "") .. "\n"
 	game.write_file("fagc-actions.txt", text, true, 0)
 end)
 
 script.on_event(defines.events.on_player_unbanned, function (e)
-	local text = "unban;" .. e.player_name .. ";" .. (e.by_player or "") .. ";" .. (e.reason or "")
+	local text = "unban;" .. e.player_name .. ";" .. (e.by_player or "") .. ";" .. (e.reason or "") .. "\n"
 	game.write_file("fagc-actions.txt", text, true, 0)
 end)
