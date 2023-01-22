5e574c39d4e55d4605bd666a83ab9d80fbebcdff
oof2win2 <honza.koco44@gmail.com>
2023-01-22 08:49:48 +0000

    feat: prep filterObjectChanged for action handling

--- apps/clientside-bot/src/utils/functions/filterObjectChangedBanlists.ts
+++ apps/clientside-bot/src/utils/functions/filterObjectChangedBanlists.ts
@@ -1,30 +1,29 @@
 import { FilterObject, Report } from "@fdgl/types"
-import { Connection } from "typeorm"
-import FDGLBan from "../../database/FDGLBan"
 import PrivateBan from "../../database/PrivateBan"
 import Whitelist from "../../database/Whitelist"
-import splitIntoGroups from "./splitIntoGroups"
+import { FDGLCategoryAction, FDGLCategoryHandler } from "../../types"
 
 /**
  * Generate the list of players to ban and unban based on a filter object change
+ *
+ * Pure function
  */
 export default async function filterObjectChangedBanlists({
+	oldFilter,
 	newFilter,
-	database,
-	validReports,
+	previouslyValidReports,
+	newlyValidReports,
+	categoryActions,
+	whitelist,
+	privateBans,
 }: {
-	/**
-	 * The new filter
-	 */
+	oldFilter: FilterObject
 	newFilter: FilterObject
-	/**
-	 * Connection to the database
-	 */
-	database: Connection
-	/**
-	 * New reports that are valid for the new config
-	 */
-	validReports: Report[]
+	previouslyValidReports: Report[]
+	newlyValidReports: Report[]
+	categoryActions: Map<string, FDGLCategoryHandler>
+	whitelist: Whitelist[]
+	privateBans: PrivateBan[]
 }) {
 	/*
 	 	- Unbanning
@@ -39,43 +38,37 @@ export default async function filterObjectChangedBanlists({
 			- Keep in mind, that some people's reports *still may be valid* in other guilds, so we need to keep track of which guilds they are valid in and not remove them from the DB outright
 	*/
 
-	const toBanPlayers = new Set<string>()
-	const toUnbanPlayers = new Set<string>()
-
-	const currentBans = await database.getRepository(FDGLBan).find()
-	const whitelist = await database.getRepository(Whitelist).find()
-	const privateBans = await database.getRepository(PrivateBan).find()
-
-	const currentlyBannedPlayers = new Set(
-		currentBans.map((ban) => ban.playername)
-	)
+	const toBanPlayers: Report[] = []
+	const toUnbanPlayers: Report[] = []
+	const toRebanPlayers: Report[] = []
+	const ignoredNames = [
+		...whitelist.map((record) => record.playername),
+		...privateBans.map((record) => record.playername),
+	]
 
 	// get all the reports into a single map
-	const bansByPlayer = new Map<string, FDGLBan[]>()
-	currentBans.forEach((ban) => {
+	const bansByPlayer = new Map<string, Report[]>()
+	previouslyValidReports.forEach((ban) => {
 		if (!bansByPlayer.has(ban.playername)) {
 			bansByPlayer.set(ban.playername, [ban])
 		} else {
 			bansByPlayer.get(ban.playername)!.push(ban)
 		}
 	})
-	validReports.forEach((report) => {
+	newlyValidReports.forEach((report) => {
 		if (!bansByPlayer.has(report.playername)) {
-			bansByPlayer.set(report.playername, [])
+			bansByPlayer.set(report.playername, [report])
 		} else {
-			bansByPlayer.get(report.playername)!.push({
-				id: report.id,
-				playername: report.playername,
-				communityId: report.communityId,
-				categoryId: report.categoryId,
-				removedAt: null,
-				createdAt: report.reportCreatedAt,
-			})
+			bansByPlayer.get(report.playername)!.push(report)
 		}
 	})
 
 	// loop over the single map and check if the player is banned in the new config
 	for (const [playername, bans] of bansByPlayer) {
+		// if the player has a privateban or whitelist against them, we don't
+		// want to take any action on them
+		if (ignoredNames.includes(playername)) continue
+
 		const newBans = bans.filter((ban) => {
 			// check if the ban is valid under the new guild filters
 			return (
@@ -83,74 +76,42 @@ export default async function filterObjectChangedBanlists({
 				newFilter.categoryFilters.includes(ban.categoryId)
 			)
 		})
+		const oldBans = bans.filter((ban) => {
+			// check if the ban is valid under the old guild filters
+			return (
+				oldFilter.communityFilters.includes(ban.communityId) &&
+				oldFilter.categoryFilters.includes(ban.categoryId)
+			)
+		})
 
-		if (newBans.length > 0) {
-			// if the player is banned in the new config, add them to the list of players to ban
-			if (!currentlyBannedPlayers.has(playername))
-				toBanPlayers.add(playername)
-		} else {
-			// unban the player
-			toUnbanPlayers.add(playername)
+		// TODO: here we would compute the set difference of the actions taken
+		const bannedBefore = oldBans.find((ban) => {
+			const action = categoryActions.get(ban.categoryId)!
+			if (action.createAction.includes(FDGLCategoryAction.FactorioBan))
+				return true
+		})
+		const bannedAfter = newBans.find((ban) => {
+			const action = categoryActions.get(ban.categoryId)!
+			if (action.createAction.includes(FDGLCategoryAction.FactorioBan))
+				return true
+		})
+
+		if (bannedBefore && !bannedAfter) {
+			toUnbanPlayers.push(bannedBefore)
+		} else if (bannedBefore && bannedAfter) {
+			toRebanPlayers.push(bannedAfter)
+		} else if (!bannedBefore && bannedAfter) {
+			toBanPlayers.push(bannedAfter)
 		}
 	}
 
 	// remove any old bans that are not in any config from the database
-	// this is done in a transaction so that the table temp.community_filters is not actually created
-	await database.transaction(async (transaction) => {
-		// create a temp table to store the filters
-		await transaction.query(
-			"CREATE TEMP TABLE `temp.community_filters` (id INTEGER PRIMARY KEY AUTOINCREMENT, communityId TEXT, categoryId TEXT);"
+	const reportsToRemoveFromDB = previouslyValidReports.filter((ban) => {
+		return (
+			!newFilter.communityFilters.includes(ban.communityId) ||
+			!newFilter.categoryFilters.includes(ban.categoryId)
 		)
-		await transaction.query(
-			`INSERT INTO \`temp.community_filters\` (communityId) VALUES ('${newFilter.communityFilters}');`
-		)
-		await transaction.query(
-			`INSERT INTO \`temp.community_filters\` (categoryId) VALUES ('${newFilter.categoryFilters}');`
-		)
-
-		// remove all the bans that are not in the filters
-		await transaction
-			.getRepository(FDGLBan)
-			.createQueryBuilder()
-			.delete()
-			.where(
-				"communityId NOT IN (SELECT communityId FROM `temp.community_filters` WHERE communityId IS NOT NULL)"
-			)
-			.orWhere(
-				"categoryId NOT IN (SELECT categoryId FROM `temp.community_filters` WHERE categoryId IS NOT NULL)"
-			)
-			.execute()
-		// drop the temp table since it's useless now
-		await transaction.query("DROP TABLE `temp.community_filters`;")
 	})
-
-	// insert valid reports into the database AFTER the above transaction
-	// since all these reports are valid, it doesn't make sense for the transaction to check them
-	// and remove them from the database
-	for (const splitReports of splitIntoGroups(validReports, 750)) {
-		await database
-			.getRepository(FDGLBan)
-			.createQueryBuilder()
-			.insert()
-			.orIgnore()
-			.values(
-				splitReports.map((report) => {
-					return {
-						id: report.id,
-						playername: report.playername,
-						communityId: report.communityId,
-						categoryId: report.categoryId,
-					}
-				})
-			)
-			.execute()
-	}
-
-	// remove any mention of players that are whitelisted or private banned from results of banning and unbanning
-	for (const record of [...whitelist, ...privateBans]) {
-		toBanPlayers.delete(record.playername)
-		toUnbanPlayers.delete(record.playername)
-	}
 
 	return {
 		/**
@@ -161,5 +122,7 @@ export default async function filterObjectChangedBanlists({
 		 * The players that are to be unbanned
 		 */
 		toUnban: [...toUnbanPlayers],
+		toReban: [...toRebanPlayers],
+		reportsToRemoveFromDB: reportsToRemoveFromDB,
 	}
 }
